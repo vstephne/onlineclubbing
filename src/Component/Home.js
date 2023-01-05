fc1b1db7c70ed575679b00c7085ce50e2b13c03c
Sandhya Kurasani <k.sandhya6070@gmail.com>
2023-01-04 22:34:23 -0500

    adding button

---
+++ src/Component/Home.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+
+class Home extends Component{
+  render(){
+    return(
+      <div>
+       <h1>Welcome to Online Clubbing</h1>
+       <button>Create Room</button>
+      </div>
+    )
+  }
+}
+
+export default  Home
